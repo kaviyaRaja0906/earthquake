539cfa5dda02f7f4fd672bb922c74349a4aa767c
kaviyaRaja0906 <kaviya@taptrackmedia.com>
2023-07-19 21:34:25 +0530

    server update

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+// server.js (or index.js, depending on your setup)
 const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
@@ -5,26 +6,61 @@ const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey.json');
 const { v4: uuidv4 } = require('uuid');
 
-const PORT = 5000;
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-// Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
-const earthquakesCollection = db.collection('earthquakes');
+const earthquakesCollection = db.collection('earthquakedatas');
 
 app.use(express.json());
 app.use(cors());
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: ["https://earthquake-e3pd.onrender.com"],
 };
 
 const requestEndpoint = 'https://data.bmkg.go.id/DataMKG/TEWS/gempadirasakan.json';
 
-app.get('/getData', cors(corsOptions), async (req, res) => {
+function convertToGeoJSON(data) {
+  const features = data.Infogempa.gempa.map((entry) => {
+    const coordinates = entry.Coordinates.split(',');
+    const longitude = parseFloat(coordinates[1]);
+    const latitude = parseFloat(coordinates[0]);
+
+    if (isNaN(longitude) || isNaN(latitude)) {
+      throw new Error('Invalid longitude or latitude');
+    }
+    const earthquakeDate = new Date(entry.DateTime);
+    return {
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+      },
+      properties: {
+        id: uuidv4(),
+        DateTime: earthquakeDate.toISOString(),
+        Date: earthquakeDate,
+        region: entry.Wilayah,
+        magnitude: parseFloat(entry.Magnitude),
+        depth: parseInt(entry.Kedalaman.match(/\d+/)[0]),
+        latitude: latitude,
+        longitude: longitude,
+      },
+    };
+  });
+
+  return {
+    type: 'FeatureCollection',
+    features,
+  };
+}
+
+
+async function fetchAndStoreEarthquakeData() {
   try {
     const fetchOptions = {
       method: 'GET',
@@ -32,92 +68,46 @@ app.get('/getData', cors(corsOptions), async (req, res) => {
     const response = await fetch(requestEndpoint, fetchOptions);
     const data = await response.json();
 
-    function convertToGeoJSON(data) {
-      const features = data.Infogempa.gempa.map((entry) => {
-        const coordinates = entry.Coordinates.split(',');
-        const longitude = parseFloat(coordinates[1]);
-        const latitude = parseFloat(coordinates[0]);
-
-        if (isNaN(longitude) || isNaN(latitude)) {
-          throw new Error('Invalid longitude or latitude');
-        }
-
-        return {
-          type: 'Feature',
-          geometry: {
-            type: 'Point',
-            coordinates: [longitude, latitude],
-          },
-          properties: {
-            id: uuidv4(),
-            dateTime: new Date(entry.DateTime),
-            region: entry.Wilayah,
-            magnitude: parseFloat(entry.Magnitude),
-            depth: parseInt(entry.Kedalaman.match(/\d+/)[0]),
-            latitude: latitude,
-            longitude: longitude,
-          },
-        };
-      });
-
-      return {
-        type: 'FeatureCollection',
-        features,
-      };
-    }
-
     const geojson = convertToGeoJSON(data);
-    res.json(geojson);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'Internal server error' });
-  }
-});
+    const { features } = geojson;
 
+    for (const feature of features) {
+      const earthquakeDateTime = feature.properties.DateTime;
 
-app.post('/storeData', async (req, res) => {
-  try {
-    const { features } = req.body;
-    const batch = db.batch();
-    if (!Array.isArray(features) || features.length === 0) {
-      throw new Error('Invalid features array');
+      const snapshot = await earthquakesCollection.where('properties.DateTime', '==', earthquakeDateTime).get();
+      if (snapshot.empty) {
+        const docRef = earthquakesCollection.doc();
+        await docRef.set(feature);
+      }
     }
-    features.forEach((feature) => {
-      const uniqueId = uuidv4();
-      const docRef = earthquakesCollection.doc(uniqueId);
-      const featureWithId = {
-        ...feature,
-        properties: {
-          ...feature.properties,
-          id: uniqueId,
-        },
-      };
-      batch.set(docRef, featureWithId);
+
+    console.log('New earthquakes added to the database:', features.length);
+  } catch (error) {
+    console.error('Error fetching or storing earthquakes:', error);
+  }
+}
+
+fetchAndStoreEarthquakeData();
+
+app.get('/earthquakes', cors(corsOptions), async (req, res) => {
+  try {
+    await fetchAndStoreEarthquakeData();
+
+    const snapshot = await earthquakesCollection.get();
+    const earthquakes = [];
+
+    snapshot.forEach((doc) => {
+      const earthquake = doc.data();
+      earthquakes.push(earthquake);
     });
 
-    await batch.commit();
-    res.sendStatus(200);
+    res.json(earthquakes);
   } catch (error) {
-    console.error('Error storing data:', error);
+    console.error('Error fetching earthquakes:', error);
     res.status(500).json({ error: 'Internal server error' });
   }
 });
-  app.get('/earthquakes', async (req, res) => {
-    try {
-      const snapshot = await earthquakesCollection.get();
-      const earthquakes = [];
-  
-      snapshot.forEach((doc) => {
-        earthquakes.push(doc.data());
-      });
-  
-      res.json(earthquakes);
-    } catch (error) {
-      console.error('Error fetching earthquakes:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    }
-  });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
